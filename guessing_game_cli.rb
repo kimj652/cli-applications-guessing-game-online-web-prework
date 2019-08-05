e4e3d764c501862f5e1cfef333e91d9d1e6cd60f
J K <kim.ju652@gmail.com>
2019-08-05 03:30:48 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,13 +2,12 @@
 def run_guessing_game
   random = rand(1..6)
   
-  input = 0
-  
-  until input == random
   puts "Enter a number(1-6): "
   input = gets.chomp
   
   if input != random
     puts "Sorry! The computer guessed #{random}"
+  elsif input == random
+  
 end
 end
